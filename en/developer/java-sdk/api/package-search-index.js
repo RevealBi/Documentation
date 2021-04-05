afb73fb098bba86e7da2beb47134e0cbf75b04cd
jayala <jayala@infragistics.com>
2021-04-05 19:07:06 -0300

    Adding JAVA API generated html and resources

---
+++ en/developer/java-sdk/api/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.infragistics.reveal.engine.init"},{"l":"com.infragistics.reveal.sdk.api"},{"l":"com.infragistics.reveal.sdk.api.model"}];updateSearchResults();
