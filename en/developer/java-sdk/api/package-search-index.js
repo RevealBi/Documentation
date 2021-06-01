35b6603b5e0a73fe25f728422c7ea1ae7d9e9e06
Konstantin Koynov <kkoynov@infragistics.com>
2021-06-01 12:53:58 +0300

    moving the java api doc to api docs repo

--- en/developer/java-sdk/api/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.infragistics.reveal.engine.init"},{"l":"com.infragistics.reveal.sdk.api"},{"l":"com.infragistics.reveal.sdk.api.model"}];updateSearchResults();
