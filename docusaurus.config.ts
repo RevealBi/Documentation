f8cf0f192a7550dd9da8ce47736db6cc75fe8362
Brian Lagunas <835562+brianlagunas@users.noreply.github.com>
2026-03-05 14:14:26 -0700

    update version for release

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,7 +6,7 @@ import replace from './plugins/remark/replace-variables';
 import apiDocs from './plugins/remark/api-docs';
 import { chatButtonSettings, searchBarSettings } from './inkeep.config';
 
-const sdkVersion = "1.8.3";
+const sdkVersion = "1.8.4";
 
 const config: Config = {
   title: 'Reveal',
